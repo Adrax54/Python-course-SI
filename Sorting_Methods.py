4fd4c0dc369418fb8dc5389a64536a8586b2c604
Adrian Wasalski <adrianmac@Adrians-MacBook-Pro.local>
2020-03-18 15:00:16 +0100

    Added Insertion

--- Sorting_Methods.py
+++ Sorting_Methods.py
@@ -12,3 +12,18 @@ def sortowanie_bąbelkowe(liczby):
 lista = [1,4,6,2,3,7,5,8]
 sortowanie_bąbelkowe(lista)
 print(lista, 'bąbelkowe')
+
+def przez_wstawianie(liczby):
+    n = len(liczby)
+    for i in range(1,n):
+        wstawianie = liczby[i]
+        j = i - 1
+        while j >= 0 and liczby[j] > wstawianie:
+            liczby[j+1] = liczby[j]
+            j = j - 1
+        liczby[j+1] = wstawianie
+    return(liczby)
+
+lista = [3,2,6,5,4,9,1,5,4]
+przez_wstawianie(lista)
+print(lista,'przez wstawianie')
